f8d3b7ca64b7d9a78b636c1de6f3f7723d8cca91
Sanskar Choubey <choubeysanskar03@gmail.com>
2020-12-05 12:52:08 +0530

    done:final commit

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 //References to the textarea,button and output div
 
 var btnTranslate = document.querySelector(".btn");
-// var inputField = document.querySelector(".input-field");
-// var outputField = document.querySelector(".output-field");
+var inputField = document.querySelector(".input-field");
+var outputField = document.querySelector(".output-field");
 
 //Declaration of some variables for further use
 
-// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-// var url = "https://api.funtranslations.com/translate/article_rewrite.json";
-// var text = inputField.value;
+var url = "https://api.funtranslations.com/translate/article_rewrite.json";
+var text = inputField.value;
 
 //URL-Generator Function
 
-// function urlGenerator(text) {
-//         return url + "?" + "text=" + inputField.value;
-//         console.log(generatedUrl);
-// }
+function urlGenerator(text) {
+        return url + "?" + "text=" + inputField.value;
+        console.log(generatedUrl);
+}
 
 //Error Handler Function
 
-// function errorHandler(error) {
-//         console.log("Some error occured",error);
-//         alert("The server is down!Sorry for the inconvience");
-// }
+function errorHandler(error) {
+        console.log("Some error occured",error);
+        alert("The server is down!Sorry for the inconvience");
+}
 
 //Function to fetch the URL
 
-// function fetchURL() {
-//         fetch(urlGenerator())
-//         .then(response => response.json())
-//         .then(JSON => {
-//                 var translatedText = JSON.contents.translated;
-//                 outputField.innerHTML = translatedText})
-//         .catch(errorHandler)
-// }
+function fetchURL() {
+        fetch(urlGenerator())
+        .then(response => response.json())
+        .then(JSON => {
+                var translatedText = JSON.contents.translated;
+                outputField.innerHTML = translatedText})
+        .catch(errorHandler)
+}
 
 //Event handler
-btnTranslate.addEventListener("Click", function btnEventHandler() {
-        console.log("clicked");     
+btnTranslate.addEventListener("click", function btnEventHandler() {
+        console.log(fetchURL());     
 })
 
